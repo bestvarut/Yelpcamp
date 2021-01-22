b97c09b2433aace0a71c2f4208f1d90df456ea06
Varut Phetsuwan <51030057+bestvarut@users.noreply.github.com>
2021-01-23 02:06:55 +0700

    Make separate handler.js
    move to Utils folder

--- Yelpcamp/app.js
+++ Yelpcamp/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate')
+const wrapAsync = require('./utils/wrapAsync');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
-const AppError = require('./AppError');
+const AppError = require('./utils/AppError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -27,18 +28,14 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }))
 
-function warpAsync(fn) {
-    return function (req, res, next) {
-        fn(req, res, next).catch(e => next(e))
-    }
-}
+
 
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/campgrounds', warpAsync(async (req, res, next) => {
+app.get('/campgrounds', wrapAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
 }))
@@ -48,39 +45,39 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', warpAsync(async (req, res, next) => {
+app.post('/campgrounds', wrapAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 
 }))
 
-app.get('/campgrounds/:id', warpAsync(async (req, res, next) => {
+app.get('/campgrounds/:id', wrapAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
 
     res.render('campgrounds/show', { campground });
 }))
 
-app.get('/campgrounds/:id/edit', warpAsync(async (req, res, next) => {
+app.get('/campgrounds/:id/edit', wrapAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
 
     res.render('campgrounds/edit', { campground });
 }))
 
-app.put('/campgrounds/:id', warpAsync(async (req, res, next) => {
+app.put('/campgrounds/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-app.delete('/campgrounds/:id', warpAsync(async (req, res, next) => {
+app.delete('/campgrounds/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
 }))
 
 app.use((err, req, res, next) => {
-    console.log(err.name);
+    console.log(`----------${err.name}----------`);
     const { status = 500, message = 'Something wan wrong' } = err;
     res.status(status).send(message);
 })
