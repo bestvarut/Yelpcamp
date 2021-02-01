18232059834fbda7104958ac7fbfa7b8752cfcad
Varut Phetsuwan <51030057+bestvarut@users.noreply.github.com>
2021-02-02 01:42:06 +0700

    Refactoring Users route
    create new users controller

--- Yelpcamp/routes/users.js
+++ Yelpcamp/routes/users.js
@@ -1,48 +1,19 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const User = require('../models/user');
+const user = require('../controllers/users')
 const passport = require('passport');
 
-const { reviewSchema } = require('../schemas.js');
-
-const AppError = require('../utils/AppError');
 const wrapAsync = require('../utils/wrapAsync');
 
 
+router.get('/register', user.renderRegister)
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-})
-router.post('/register', wrapAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err)
-            req.flash('success', 'Welcome to Yelp Camp!')
-            res.redirect('/campgrounds')
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register')
-    }
-}))
+router.post('/register', wrapAsync(user.register))
 
-router.get('/login', (req, res) => {
-    res.render('users/login')
-})
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'Welcome back!')
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl)
-})
+router.get('/login', user.renderLogin)
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Logout succesfully')
-    res.redirect('/campgrounds');
-})
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), user.login)
+
+router.get('/logout', user.logout)
 
 module.exports = router;
