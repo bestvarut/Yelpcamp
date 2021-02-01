37e0f9795c0118863f711ed579b27ea40e3c6172
Varut Phetsuwan <51030057+bestvarut@users.noreply.github.com>
2021-02-02 01:32:58 +0700

    Refactoring reviews route
    create new reviews controller

--- Yelpcamp/routes/campgrounds.js
+++ Yelpcamp/routes/campgrounds.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const campground = require('../controllers/campgrounds')
-const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
 
 const wrapAsync = require('../utils/wrapAsync');
--- Yelpcamp/routes/reviews.js
+++ Yelpcamp/routes/reviews.js
@@ -1,30 +1,13 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const Campground = require('../models/campground');
-const Review = require('../models/review');
+const review = require('../controllers/reviews')
 const { isLoggedIn, validateReview, isReviewAuthor } = require("../middleware");
 
 const wrapAsync = require('../utils/wrapAsync');
 
 
+router.post('/', validateReview, isLoggedIn, wrapAsync(review.createReview))
 
-router.post('/', validateReview, isLoggedIn, wrapAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Successfully create a new review!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, wrapAsync(async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully delete a review!')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, wrapAsync(review.deleteReview))
 
 module.exports = router;
